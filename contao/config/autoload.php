896d443b2de814d05808d933b490aaea5cfe6b03
Martin Kozianka <martin@kozianka.de>
2015-07-15 23:21:04 +0200

    Changed folder-gallery to folder_gallery

--- contao/config/autoload.php
+++ contao/config/autoload.php
@@ -8,23 +8,23 @@
  * PHP version 5
  * @copyright  Martin Kozianka 2013-2015 <http://kozianka.de/>
  * @author     Martin Kozianka <http://kozianka.de/>
- * @package    folder-gallery
+ * @package    folder_gallery
  * @license    LGPL 
  * @filesource
  */
 
 ClassLoader::addClasses(array
 (
-    'FolderGallery'               => 'system/modules/folder-gallery/classes/FolderGallery.php',
-	'FolderGalleryModule'         => 'system/modules/folder-gallery/modules/FolderGalleryModule.php',
+    'FolderGallery'               => 'system/modules/folder_gallery/classes/FolderGallery.php',
+	'FolderGalleryModule'         => 'system/modules/folder_gallery/modules/FolderGalleryModule.php',
 
-    'FolderGalleryCategoryModel'  => 'system/modules/folder-gallery/models/FolderGalleryCategoryModel.php',
-    'FolderGalleryModel'          => 'system/modules/folder-gallery/models/FolderGalleryModel.php',
+    'FolderGalleryCategoryModel'  => 'system/modules/folder_gallery/models/FolderGalleryCategoryModel.php',
+    'FolderGalleryModel'          => 'system/modules/folder_gallery/models/FolderGalleryModel.php',
 ));
 
 TemplateLoader::addFiles(array
 (
-    'mod_folder_gallery'          => 'system/modules/folder-gallery/templates',
-    'fg_category_default'         => 'system/modules/folder-gallery/templates',
-    'fg_gallery_default'          => 'system/modules/folder-gallery/templates',
+    'mod_folder_gallery'          => 'system/modules/folder_gallery/templates',
+    'fg_category_default'         => 'system/modules/folder_gallery/templates',
+    'fg_gallery_default'          => 'system/modules/folder_gallery/templates',
 ));
--- contao/config/config.php
+++ contao/config/config.php
@@ -8,7 +8,7 @@
  * PHP version 5
  * @copyright  Martin Kozianka 2013-2015 <http://kozianka.de/>
  * @author     Martin Kozianka <http://kozianka.de/>
- * @package    folder-gallery
+ * @package    folder_gallery
  * @license    LGPL 
  * @filesource
  */
