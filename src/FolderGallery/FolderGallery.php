111434e40cdd4b56032624226545f664e24e4098
Martin Kozianka <martin@kozianka.de>
2015-11-20 14:34:25 +0100

    Bugfixes

--- src/FolderGallery/FolderGallery.php
+++ src/FolderGallery/FolderGallery.php
@@ -13,48 +13,55 @@
  * @filesource
  */
 
-class FolderGallery extends \System {
+use FolderGallery\Models\FolderGalleryModel;
+use FolderGallery\Models\FolderGalleryCategoryModel;
 
-    public function __construct() {
+class FolderGallery extends \System
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->import('Database');
     }
 
-    public function syncGalleryCategory(DataContainer $dc) {
-
+    public function syncGalleryCategory(\DataContainer $dc)
+    {
         // Sync file system
         Dbafs::syncFiles();
 
         $this->cleanup();
 
-        $catObj  = \FolderGalleryCategoryModel::findByPk($dc->id);
+        $catObj  = FolderGalleryCategoryModel::findByPk($dc->id);
         $rootObj = \FilesModel::findByUuid($catObj->root_folder);
 
-        $existing_galleries = array();
+        $existing_galleries = [];
         $result = $this->Database->prepare('SELECT * FROM tl_folder_gallery WHERE pid = ?')
             ->execute($catObj->id);
 
-        while($result->next()) {
+        while($result->next())
+        {
             $existing_galleries[$result->folder] = $result->row();
         }
 
         $objSubfiles = \FilesModel::findMultipleByBasepath($rootObj->path.'/');
 
-        while ($objSubfiles->next()) {
-
-            if (array_key_exists($objSubfiles->path, $existing_galleries)) {
+        while ($objSubfiles->next())
+        {
+            if (array_key_exists($objSubfiles->path, $existing_galleries))
+            {
                 unset($existing_galleries[$objSubfiles->path]);
             }
-            else if ($objSubfiles->type === 'folder' && $this->hasImages($objSubfiles) ) {
-
+            else if ($objSubfiles->type === 'folder' && $this->hasImages($objSubfiles))
+            {
                 $alias    = standardize(String::restoreBasicEntities($objSubfiles->name));
                 $objAlias = $this->Database->prepare('SELECT id FROM tl_folder_gallery WHERE alias = ?')
                 ->execute($alias);
-                if ($objAlias->numRows === 1) {
+                if ($objAlias->numRows === 1)
+                {
                     $alias .= '-' . time();
                 }
 
-                $gal = array(
+                $gal = [
                     'tstamp'   => time(),
                     'pid'      => $catObj->id,
                     'title'    => $objSubfiles->name,
@@ -62,29 +69,34 @@ class FolderGallery extends \System {
                     'folder'   => $objSubfiles->path,
                     'uuid'     => $objSubfiles->uuid,
                     'datim'    => $this->getDateFromImage($objSubfiles)
-                );
+                ];
                 $this->Database->prepare("INSERT INTO tl_folder_gallery %s")->set($gal)->execute();
             }
         }
         $this->cleanup($existing_galleries);
 
-        Controller::redirect(Environment::get('script').'?do=folder_gallery&table=tl_folder_gallery&id='.$catObj->id);
+        \Controller::redirect(Environment::get('script').'?do=folder_gallery&table=tl_folder_gallery&id='.$catObj->id);
     }
 
     private function hasImages($objFile) {
 
-        if ($objFile->type === 'file') {
+        if ($objFile->type === 'file')
+        {
             return false;
         }
         $objChild  = \FilesModel::findMultipleFilesByFolder($objFile->path);
-        if ($objChild === null) {
+        if ($objChild === null)
+        {
             return false;
         }
 
-        while ($objChild->next()) {
-            if ($objChild->type === 'file') {
+        while ($objChild->next())
+        {
+            if ($objChild->type === 'file')
+            {
                 $objFile = new \File($objChild->path, true);
-                if ($objFile->isGdImage) {
+                if ($objFile->isGdImage)
+                {
                     return true;
                 }
             }
@@ -92,24 +104,30 @@ class FolderGallery extends \System {
         return false;
     }
 
-    private function getDateFromImage($objFile) {
-
+    private function getDateFromImage($objFile)
+    {
         $withExif   = function_exists('exif_read_data');
         $objChild   = \FilesModel::findMultipleByBasepath($objFile->path);
 
         // Versuche es bei X Bildern die EXIF-Daten auszulesen
         $tryCount   = 5;
 
-        if ($objChild === null) {
+        if ($objChild === null)
+        {
             return time();
         }
-        while ($objChild->next()) {
-            if ($objChild->type === 'file' && $withExif) {
+
+        while ($objChild->next())
+        {
+            if ($objChild->type === 'file' && $withExif)
+            {
                 $objFile = new \File($objChild->path, true);
-                if ($objFile->isGdImage && $tryCount > 0) {
+                if ($objFile->isGdImage && $tryCount > 0)
+                {
                     $tryCount--;
                     $exifData = exif_read_data(TL_ROOT.'/'.$objChild->path);
-                    if ($exifData !== false && array_key_exists('DateTimeOriginal', $exifData)) {
+                    if ($exifData !== false && array_key_exists('DateTimeOriginal', $exifData))
+                    {
                         return strtotime($exifData['DateTimeOriginal']);
                     }
                     return time();
@@ -120,30 +138,35 @@ class FolderGallery extends \System {
 
     }
 
-    private function cleanup() {
-
+    private function cleanup()
+    {
         // Delete or move galleries
-        $categories = array();
+        $categories = [];
         $catObj     = FolderGalleryCategoryModel::findAll();
-        while ($catObj->next()) {
+        while ($catObj->next())
+        {
             $rootObj = \FilesModel::findByUuid($catObj->root_folder);
             $categories[$catObj->id] = $rootObj->path;
         }
         $galObj = FolderGalleryModel::findAll();
-        if ($galObj === null) {
+        if ($galObj === null)
+        {
             return false;
         }
 
-        while ($galObj->next()) {
+        while ($galObj->next())
+        {
             $fileObj = \FilesModel::findByUuid($galObj->uuid);
-            if ($galObj->folder != $fileObj->path) {
-                echo '<pre>'.$galObj->folder.' != '.$fileObj->path.'<br></pre>';
+            if ($galObj->folder != $fileObj->path)
+            {
+                //  echo '<pre>'.$galObj->folder.' != '.$fileObj->path.'<br></pre>';
                 $moved = false;
-                foreach($categories as $pid => $basepath) {
+                foreach($categories as $pid => $basepath)
+                {
+                    // var_dump($pid,$basepath, $fileObj->path, strpos($fileObj->path, $basepath));
 
-                    var_dump($pid,$basepath, $fileObj->path, strpos($fileObj->path, $basepath));
-
-                    if (strpos($fileObj->path, $basepath) === 0) {
+                    if (strpos($fileObj->path, $basepath) === 0)
+                    {
                         // MOVE
                         $moved = true;
                         $this->Database->prepare("UPDATE tl_folder_gallery
@@ -151,7 +174,8 @@ class FolderGallery extends \System {
                             ->execute($pid, $fileObj->path, $galObj->id);
                     }
                 }
-                if (!$moved) {
+                if (!$moved)
+                {
                     // DELETE
                     $this->Database->prepare("DELETE FROM tl_folder_gallery WHERE id = ?")
                         ->execute($galObj->id);
